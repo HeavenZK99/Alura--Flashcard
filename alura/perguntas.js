de681c884f73795126c5d6df8976c237e196a9ad
HeavenZK99 <00001109972751SP@al.educacao.sp.gov.br>
2024-10-23 10:31:47 -0300

    Add files via upload

---
+++ alura/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Banco de Dados',
+    'O que é um banco de dados?',
+    'É um sistema que armazena dados de forma organizada.'
+)
+
+criaCartao(
+    'Segurança da Informação',
+    'O que é um firewall?',
+    'É uma barreira de segurança que controla o tráfego de rede.'
+)
+
+criaCartao(
+    'Programação Front-End',
+    'O que é front-end?',
+    'Front-end é a parte de um aplicativo web que interage com o usuário, envolvendo HTML, CSS e JavaScript. Tecnologias populares incluem React, Angular e Vue.js.'
+)
+
+criaCartao(
+    'Programação Back-End',
+    'O que é back-end?',
+    ' Back-end é a parte do aplicativo que roda no servidor, gerenciando a lógica de negócios e interagindo com o banco de dados. Tecnologias comuns incluem Node.js, Python e PHP.'
+)
